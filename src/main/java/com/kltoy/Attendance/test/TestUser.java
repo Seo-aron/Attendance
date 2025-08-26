b92fc0ccb17977dfec598c7ea3dae97749566b99
Seo-aron <dkfhs4337@naver.com>
2025-08-26 15:03:05 +0900

    근태관리 사이드 프로젝트

---
+++ src/main/java/com/kltoy/Attendance/test/TestUser.java
@@ -0,0 +1,19 @@
+package com.kltoy.Attendance.test;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "test_user")
+public class TestUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int age;
+}
---
+++ src/main/java/com/kltoy/Attendance/test/TestUserRepository.java
@@ -0,0 +1,6 @@
+package com.kltoy.Attendance.test;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestUserRepository extends JpaRepository<TestUser, Long> {
+}
